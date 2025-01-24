9a4add140b23f0b2f6f0631acd500a0e8e0cc765
zyeonK <zyeon@snu.ac.kr>
2025-01-24 23:56:12 +0900

    Update behav_code4_autocorrelation.m

--- code/behav_code4_autocorrelation.m
+++ code/behav_code4_autocorrelation.m
@@ -1,7 +1,7 @@
 rng(918)
 
 %% Set directory
-data_dir = '../NC_data';
+data_dir = '../data';
 data_name  = 'group_pattern_data.mat';
 %% Load data
 load(fullfile(data_dir,data_name)) % avg_data, tmap_all
